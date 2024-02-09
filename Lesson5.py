0da2287af88a3bdaaaefbf536766d4b91e1a85f5
Hisanori Shimakawa <shimakawa.hisanori@gmail.com>
2024-02-10 01:54:35 +0900

    Softmaxレイヤ & Lossレイヤ

--- Lesson5.py
+++ Lesson5.py
@@ -1,4 +1,5 @@
 import numpy as np
+from Lesson4 import softmax, cross_entropy_error
 
 """
 乗算レイヤ
@@ -150,3 +151,24 @@ class Affine:
 
         return dx
     
+"""
+SoftmaxWithLossレイヤ
+"""
+class SoftmaxWithLoss:
+    def __init__ (self):
+        self.loss = None # 損失
+        self.y = None # 推論値
+        self.t = None # 教師データ
+
+    def forward(self, x, t):
+        self.t = t
+        self.y = softmax(x)
+        self.loss = cross_entropy_error(self.y, self.t)
+
+        return self.loss
+    
+    def backward(self, dout=1):
+        batch_size = self.t.shape[0]
+        dx = (self.y - self.t) / batch_size
+
+        return dx
