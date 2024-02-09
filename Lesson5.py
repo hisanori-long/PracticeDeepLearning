db1754c9cd674d03bfdbeb515fe62c1511034b71
Hisanori Shimakawa <shimakawa.hisanori@gmail.com>
2024-02-10 01:47:29 +0900

    Affineレイヤ

--- Lesson5.py
+++ Lesson5.py
@@ -125,3 +125,28 @@ class Sigmoid:
         dx = dout * (1.0 - self.out) * self.out
 
         return dx
+    
+"""
+Affineレイヤ
+"""
+class Affine:
+    def __init__(self, W, b):
+        self.W = W
+        self.b = b
+        self.x = None
+        self.dW = None
+        self.db = None
+
+    def forward(self, x):
+        self.x = x
+        out = np.dot(x, self.W) + self.b
+
+        return out
+    
+    def backward(self, dout):
+        dx = np.dot(dout, self.W.T) # W.TはWの転置行列
+        self.dW = np.dot(self.x.T, dout)
+        self.db = np.sum(dout, axis=0)
+
+        return dx
+    
