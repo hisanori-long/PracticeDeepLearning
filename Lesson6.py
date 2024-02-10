2c33fc73d17ced45aafbbd92ec2d1bc8df99635c
Hisanori Shimakawa <shimakawa.hisanori@gmail.com>
2024-02-10 21:51:37 +0900

    AdaGrad

--- Lesson6.py
+++ Lesson6.py
@@ -18,3 +18,21 @@ class Momentum:
         for key in params.keys():
             self.v[key] = self.momentum * self.v[key] - self.le * grads[key]
             params[key] += self.v[key]
+
+"""
+AdaGrad
+"""
+class AdaGrad:
+    def __init__(self, lr=0.01):
+        self.lr = lr
+        self.h = None
+
+    def update(self, params, grads):
+        if self.h is None: # 初期設定
+            self.h = {}
+            for key, val in params.items():
+                self.h[key] = np.zeros_like(val)
+
+        for key in params.keys():
+            self.h[key] += grads[key] * grads[key] # hに勾配の二乗を加算し、更新
+            params[key] -= self.lr * grads[key] / (np.sqrt(self.h[key]) + 1e-7) # 1e-7は0除算を防ぐための微小値
