96585d714fa9ee27d5a49a03072ec2c6817ced56
Hisanori Shimakawa <shimakawa.hisanori@gmail.com>
2024-02-10 22:30:48 +0900

    過学習の検証

--- Lesson6.py
+++ Lesson6.py
@@ -1,5 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from dataset.mnist import load_mnist
+from common.multi_layer_net import MultiLayerNet
 
 """
 Momentum
@@ -72,4 +74,46 @@ for i, a in activations.items():
     plt.subplot(1, len(activations), i+1)
     plt.title(str(i+1) + "-layer")
     plt.hist(a.flatten(), 30, range=(0,1))
-plt.show()
+# plt.show()
+
+"""
+過学習
+"""
+(x_train, t_train), (x_test, t_test) = load_mnist(normalize=True)
+# 過学習を再現するために、学習データを削減
+x_train = x_train[:300]
+t_train = t_train[:300]
+
+network = MultiLayerNet(input_size=784, hidden_size_list=[100, 100, 100, 100, 100, 100], output_size=10)
+optimizer = AdaGrad(lr=0.01) # 学習係数が0.01の場合
+
+max_epochs = 201
+train_size = x_train.shape[0]
+batch_size = 100
+
+train_loss_list = []
+train_acc_list = []
+test_acc_list = []
+
+iter_per_epoch = max(train_size / batch_size, 1)
+epoch_cnt = 0
+
+for i in range(1000000000):
+    batch_mask = np.random.choice(train_size, batch_size)
+    x_batch = x_train[batch_mask]
+    t_batch = t_train[batch_mask]
+
+    grads = network.gradient(x_batch, t_batch)
+    optimizer.update(network.params, grads)
+
+    if i % iter_per_epoch == 0:
+        train_acc = network.accuracy(x_train, t_train)
+        test_acc = network.accuracy(x_test, t_test)
+        train_acc_list.append(train_acc)
+        test_acc_list.append(test_acc)
+
+        print("epoch:" + str(epoch_cnt) + ", train acc:" + str(train_acc) + ", test acc:" + str(test_acc))
+
+        epoch_cnt += 1
+        if epoch_cnt >= max_epochs:
+            break
