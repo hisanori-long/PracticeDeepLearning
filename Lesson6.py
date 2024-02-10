afcf76375f2d765bc5167619bb12062a41d439c8
Hisanori Shimakawa <shimakawa.hisanori@gmail.com>
2024-02-10 22:19:04 +0900

    アクティベーション分布

--- Lesson6.py
+++ Lesson6.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 """
 Momentum
@@ -36,3 +37,39 @@ class AdaGrad:
         for key in params.keys():
             self.h[key] += grads[key] * grads[key] # hに勾配の二乗を加算し、更新
             params[key] -= self.lr * grads[key] / (np.sqrt(self.h[key]) + 1e-7) # 1e-7は0除算を防ぐための微小値
+
+
+"""
+アクティベーションの分布を確認
+"""
+# 重みの初期値の分散によって、アクティベーションの分布がどのように変化するかを確認
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+def tanh(x):
+    return np.tanh(x)
+
+x = np.random.randn(1000, 100) # 1000個のデータ
+node_num =100 # 各隠れ層のノード（ニューロン）の数
+hidden_layer_size = 5 # 隠れ層が5層
+activations = {} # ここにアクティベーションの結果を格納
+
+for i in range(hidden_layer_size):
+    if i != 0:
+        x = activations[i-1]
+
+    # w = np.random.randn(node_num, node_num) * 1 # 標準偏差が1
+    # w = np.random.randn(node_num, node_num) * 0.01 # 標準偏差が0.01
+    w = np.random.randn(node_num, node_num) / np.sqrt(node_num) # Xavierの初期値
+    
+    z = np.dot(x, w)
+    a = sigmoid(z)
+    # a = tanh(z)
+    activations[i] = a
+
+# ヒストグラムを描画
+for i, a in activations.items():
+    plt.subplot(1, len(activations), i+1)
+    plt.title(str(i+1) + "-layer")
+    plt.hist(a.flatten(), 30, range=(0,1))
+plt.show()
