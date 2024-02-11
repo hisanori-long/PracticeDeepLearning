3e35044cf95702f6d16a0feb867158c36fc5c1b7
Hisanori Shimakawa <shimakawa.hisanori@gmail.com>
2024-02-12 00:43:08 +0900

    CNNの実装

--- Lesson7.py
+++ Lesson7.py
@@ -6,6 +6,8 @@ import sys, os
 sys.path.append(os.pardir)
 from common.util import im2col, col2im
 import numpy as np
+from collections import OrderedDict
+from Lesson5 import Relu, Affine, SoftmaxWithLoss
 
 """
 Convolution（畳み込み）レイヤ
@@ -90,3 +92,71 @@ class Pooling:
         dx = col2im(dcol, self.x.shape, self.pool_h, self.pool_w, self.stride, self.pad) # 2次元配列を4次元配列に変換
 
         return dx
+    
+"""
+CNNの実装
+"""
+class SimpleConvNet:
+    def __init__(self, input_dim=(1, 28, 28), conv_param={'filter_num':30, 'filter_size':5, 'pad':0, 'stride':1}, hidden_size=100, output_size=10, weight_init_std=0.01):
+        filter_num = conv_param['filter_num'] # フィルターの数
+        filter_size = conv_param['filter_size'] # フィルターのサイズ
+        filter_pad = conv_param['pad'] # パディング
+        filter_stride = conv_param['stride'] # ストライド
+        input_size = input_dim[1]
+        conv_output_size = (input_size - filter_size + 2 * filter_pad) / filter_stride + 1 # 畳み込み層の出力サイズ
+        pool_output_size = int(filter_num * (conv_output_size / 2) * (conv_output_size / 2)) # プーリング層の出力サイズ
+
+        # 重みの初期化
+        self.params = {}
+        self.params['W1'] = weight_init_std * np.random.randn(filter_num, input_dim[0], filter_size, filter_size)
+        self.params['b1'] = np.zeros(filter_num)
+        self.params['W2'] = weight_init_std * np.random.randn(pool_output_size, hidden_size)
+        self.params['b2'] = np.zeros(hidden_size)
+        self.params['W3'] = weight_init_std * np.random.randn(hidden_size, output_size)
+        self.params['b3'] = np.zeros(output_size)
+
+        # レイヤの生成
+        self.layers = OrderedDict()
+        self.layers['Conv1'] = Convolution(self.params['W1'], self.params['b1'], conv_param['stride'], conv_param['pad'])
+        self.layers['Relu1'] = Relu()
+        self.layers['Pool1'] = Pooling(pool_h=2, pool_w=2, stride=2)
+        self.layers['Affine1'] = Affine(self.params['W2'], self.params['b2'])
+        self.layers['Relu2'] = Relu()
+        self.layers['Affine2'] = Affine(self.params['W3'], self.params['b3'])
+        self.last_layer = SoftmaxWithLoss()
+
+    def predict(self, x):
+        for layer in self.layers.values():
+            x = layer.forward(x)
+        
+        return x
+    
+    def loss(self, x, t):
+        y = self.predict(x)
+        return self.lastLayer.forward(y, t)
+    
+    def gradient(self, x, t):
+        # forward
+        self.loss(x, t)
+
+        # backward
+        dout = 1
+        dout = self.last_layer.backward(dout)
+
+        layers = list(self.layers.values())
+        layers.reverse()
+        for layer in layers:
+            dout = layer.backward(dout)
+
+        # 設定
+        grads = {}
+        grads['W1'] = self.layers['Conv1'].dW
+        grads['b1'] = self.layers['Conv1'].db
+        grads['W2'] = self.layers['Affine1'].dW
+        grads['b2'] = self.layers['Affine1'].db
+        grads['W3'] = self.layers['Affine2'].dW
+        grads['b3'] = self.layers['Affine2'].db
+
+        return grads
+    
+    
